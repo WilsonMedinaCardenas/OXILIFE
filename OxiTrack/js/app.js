014f29cb9e74000b51b8128e2524a7a4e7b11a99
Wilson Medina <wilson.89medina@gmail.com>
2026-07-25 12:02:55 -0400

    Agregar OxiTrack22.8

--- OxiTrack/js/app.js
+++ OxiTrack/js/app.js
@@ -150,7 +150,7 @@ document.getElementById("formulario").addEventListener("submit", async (e) => {
      // CAPTURA EL EMAIL OCULTO POR DETRÁS EN BASE AL NOMBRE SELECCIONADO
     const mapeoCliente = baseDatosClientes.find(item => item.nombre.trim() === cliente.trim());
     const emailDetectado = mapeoCliente ? mapeoCliente.email : "";
-    alert("🔍 DIAGNÓSTICO: El sistema detectó para " + cliente + " el correo: " + (emailDetectado || "VACÍO/NO ENCONTRADO"));
+    // alert("🔍 DIAGNÓSTICO: El sistema detectó para " + cliente + " el correo: " + (emailDetectado || "VACÍO/NO ENCONTRADO"));
 
 
 
